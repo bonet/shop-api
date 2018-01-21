061a6ad842a426b5d4fa6040abab92186263edee
Bonet Sugiarto <bonetsu@gmail.com>
2018-01-21 23:55:24 +0800

    Add show

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,8 @@ class ProductsController < ApplicationController
   def index
     render json: ProductsDatatable.new(params).as_json
   end
+
+  def show
+    render json: Product.where(id: params[:id]).last
+  end
 end
