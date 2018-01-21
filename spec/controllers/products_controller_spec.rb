6299a5d38efa55ee8e0353d20182513afa702d3f
Bonet Sugiarto <bonetsu@gmail.com>
2018-01-22 00:44:04 +0800

    Add controller show test

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -5,17 +5,32 @@ RSpec.describe ProductsController, type: :controller do
     it "returns correct basic product data" do
       product_1 = create(:product, category: "books", sold_out: false, under_sale: false)
       product_2 = create(:product, category: "shoes", sold_out: false, under_sale: true)
-      products = Product.order("price DESC").map(&:attributes)
+      products = Product.where(category: "shoes").order("price DESC").map(&:attributes)
 
       @request.headers['HTTP_ACCEPT'] = 'application/json'
-      get :index, params: { order_by: :price }
+      get :index, params: { order_by: :price, category: "shoes"}
 
       parsed_body = JSON.parse(response.body)
+      prods = parsed_body["data"]
+      expect(products).to eq prods
+      expect(response.status).to eq 200
+      expect(parsed_body["recordsTotal"]).to eq Product.count
+      expect(parsed_body["recordsFiltered"]).to eq products.count
+    end
+  end
+
+  describe "GET show json" do
+    it "returns correct basic product data" do
+      product_1 = create(:product, category: "books", sold_out: false, under_sale: false)
+      product_2 = create(:product, category: "shoes", sold_out: false, under_sale: true)
+
+      @request.headers['HTTP_ACCEPT'] = 'application/json'
+      get :show, params: { id: product_1.id }
+
+      product = JSON.parse(response.body)
       
       expect(response.status).to eq 200
-      expect(parsed_body["data"]).to eq products
-      expect(parsed_body["recordsTotal"]).to eq products.count
-      expect(parsed_body["recordsFiltered"]).to eq products.count
+      expect(product).to eq product_1.as_json
     end
   end
 end
